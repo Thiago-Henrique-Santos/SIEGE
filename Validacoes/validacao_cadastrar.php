dd72bb555d38e48f7a2217b1101301bd721f190f
Thiago Santos <thilego418@gmail.com>
2021-05-21 23:26:05 -0300

    Corrigindo inputs e reoganizando os formulário de cadastro de diretores, supervisores, secretários e professores

---
+++ Validacoes/validacao_cadastrar.php
@@ -0,0 +1,14 @@
+<?php
+
+    if (!isset($_GET['cargo'])){
+        echo "Erro!";
+    }
+
+    switch ($_GET['cargo']) {
+        case "diretor":
+            
+            break;
+        case ""
+    }
+
+?>
--- formularios-cadastro.php
+++ formularios-cadastro.php
@@ -32,8 +32,11 @@ if (!isset($_GET['id'])){
         echo "</div>";
     }
 
-    function sup_sec_dir() {
-        echo "<form method='POST' action='validacao_cadastros.php'>";
+    function diretor() {
+        echo "<h1 class='titulo_medio'>Cadastro de Diretor</h1>";
+        echo "<form method='POST' action='Validacoes/validacao_cadastrar.php'>";
+            echo "<input type='text' style='display: none;' id='cargo' name='cargo' value='diretor'>";
+
             echo "<br><label for='nome_completo' class='form-label'>Nome completo: </label>";
                 echo "<input type='text' required class='form-control' id='nome_completo' name='nome_completo' value=''>";
 
@@ -61,14 +64,152 @@ if (!isset($_GET['id'])){
                 echo "<input type='radio' name='campo_s' id='campo_sF' value=''>";
             echo "<label for='campo_sF'>Feminino</label> <br><br>";
 
-            echo "<br><label for='cargo_funcao' class='form-label'>Cargo ou função: </label>";
-                echo "<input type='text' required class='form-control' id='cargo_funcao' name='cargo_funcao' value=''>";
+            echo "<br><label for='funcao' class='form-label'>Função: </label>";
+                echo "<input type='text' required class='form-control' id='funcao' name='funcao' value=''>";
 
             echo "<br><label for='senha' class='form-label'>Senha: </label>";
                 echo "<input type='password' class='form-control' maxlength = '6' required name='senha' value=''>";
 
             echo "<br><label for='confirm_senha' class='form-label'>Confirme a senha: </label>";
                 echo "<input type='password' class='form-control' maxlength = '6' required name='confirm_senha' value=''>";
+
+            echo "<input class='btn btn-primary' type='submit' name='botao' value='Confirmar'>";
+        echo "</form>";
+    }
+
+    function supervisor() {
+        echo "<h1 class='titulo_medio'>Cadastro de Supervisores</h1>";
+        echo "<form method='POST' action='Validacoes/validacao_cadastrar.php'>";
+            echo "<input type='text' style='display: none;' id='cargo' name='cargo' value='supervisor'>";
+
+            echo "<br><label for='nome_completo' class='form-label'>Nome completo: </label>";
+                echo "<input type='text' required class='form-control' id='nome_completo' name='nome_completo' value=''>";
+
+            echo "<br><label for='masp' class='form-label'>MASP: </label>";
+                echo "<input type='text' maxlength = '8' required class='form-control' id='masp' name='masp' value=''>";
+
+            echo "<br><label for='email' class='form-label'>Email: </label>";
+                echo "<input type='email' class='form-control' minlength = '5' required pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' title='O email deve conter @ e . ao final.' id='senha' name='senha' value=''>";
+
+            echo "<br><label for='campo_zona'>Local de moradia: </label><br>";
+                echo "<input type='radio' name='campo_zona' id='campo_zR' value=''>";
+            echo "<label for='campo_zR'>Zona rural</label>";
+                echo "<input type='radio' name='campo_zona' id='campo_zU' value=''>";
+            echo "<label for='campo_zU'>Zona urbana</label> <br><br>";
+
+            echo "<br><label for='tipo_empregado'>Tipo de empregado: </label><br>";
+                echo "<input type='radio' name='tipo_empregado' id='designado' value=''>";
+            echo "<label for='designado'>Designado</label>";
+                echo "<input type='radio' name='tipo_empregado' id='efetivo' value=''>";
+            echo "<label for='efetivo'>Efetivo</label> <br><br>";
+
+            echo "<br><label for='campo_s'>Sexo: </label><br>";
+                echo "<input type='radio' name='campo_s' id='campo_sM' value=''>";
+            echo "<label for='campo_sM'>Masculino</label>";
+                echo "<input type='radio' name='campo_s' id='campo_sF' value=''>";
+            echo "<label for='campo_sF'>Feminino</label> <br><br>";
+
+            echo "<br><label for='funcao' class='form-label'>Função: </label>";
+                echo "<input type='text' required class='form-control' id='funcao' name='funcao' value=''>";
+
+            echo "<br><label for='senha' class='form-label'>Senha: </label>";
+                echo "<input type='password' class='form-control' maxlength = '6' required name='senha' value=''>";
+
+            echo "<br><label for='confirm_senha' class='form-label'>Confirme a senha: </label>";
+                echo "<input type='password' class='form-control' maxlength = '6' required name='confirm_senha' value=''>";
+            
+            echo "<input class='btn btn-primary' type='submit' name='botao' value='Confirmar'>";
+        echo "</form>";
+    }
+
+    function professor() {
+        echo "<h1 class='titulo_medio'>Cadastro de Professores</h1>";
+        echo "<form method='POST' action='Validacoes/validacao_cadastrar.php'>";
+            echo "<input type='text' style='display: none;' id='cargo' name='cargo' value='professor'>";
+
+            echo "<br><label for='nome_completo' class='form-label'>Nome completo: </label>";
+                echo "<input type='text' required class='form-control' id='nome_completo' name='nome_completo' value=''>";
+
+            echo "<br><label for='masp' class='form-label'>MASP: </label>";
+                echo "<input type='text' maxlength = '8' required class='form-control' id='masp' name='masp' value=''>";
+
+            echo "<br><label for='email' class='form-label'>Email: </label>";
+                echo "<input type='email' class='form-control' minlength = '5' required pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' title='O email deve conter @ e . ao final.' id='senha' name='senha' value=''>";
+
+            echo "<br><label for='campo_zona'>Local de moradia: </label><br>";
+                echo "<input type='radio' name='campo_zona' id='campo_zR' value=''>";
+            echo "<label for='campo_zR'>Zona rural</label>";
+                echo "<input type='radio' name='campo_zona' id='campo_zU' value=''>";
+            echo "<label for='campo_zU'>Zona urbana</label> <br><br>";
+
+            echo "<br><label for='tipo_empregado'>Tipo de empregado: </label><br>";
+                echo "<input type='radio' name='tipo_empregado' id='designado' value=''>";
+            echo "<label for='designado'>Designado</label>";
+                echo "<input type='radio' name='tipo_empregado' id='efetivo' value=''>";
+            echo "<label for='efetivo'>Efetivo</label> <br><br>";
+
+            echo "<br><label for='campo_s'>Sexo: </label><br>";
+                echo "<input type='radio' name='campo_s' id='campo_sM' value=''>";
+            echo "<label for='campo_sM'>Masculino</label>";
+                echo "<input type='radio' name='campo_s' id='campo_sF' value=''>";
+            echo "<label for='campo_sF'>Feminino</label> <br><br>";
+
+            echo "<br><label for='funcao' class='form-label'>Função: </label>";
+                echo "<input type='text' required class='form-control' id='funcao' name='funcao' value=''>";
+
+            echo "<br><label for='senha' class='form-label'>Senha: </label>";
+                echo "<input type='password' class='form-control' maxlength = '6' required name='senha' value=''>";
+
+            echo "<br><label for='confirm_senha' class='form-label'>Confirme a senha: </label>";
+                echo "<input type='password' class='form-control' maxlength = '6' required name='confirm_senha' value=''>";
+
+            echo "<input class='btn btn-primary' type='submit' name='botao' value='Confirmar'>";
+        echo "</form>";
+    }
+
+    function secretario() {
+        echo "<h1 class='titulo_medio'>Cadastro de Secretários</h1>";
+        echo "<form method='POST' action='Validacoes/validacao_cadastrar.php'>";
+            echo "<input type='text' style='display: none;' id='cargo' name='cargo' value='secretario'>";
+
+            echo "<br><label for='nome_completo' class='form-label'>Nome completo: </label>";
+                echo "<input type='text' required class='form-control' id='nome_completo' name='nome_completo' value=''>";
+
+            echo "<br><label for='masp' class='form-label'>MASP: </label>";
+                echo "<input type='text' maxlength = '8' required class='form-control' id='masp' name='masp' value=''>";
+
+            echo "<br><label for='email' class='form-label'>Email: </label>";
+                echo "<input type='email' class='form-control' minlength = '5' required pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' title='O email deve conter @ e . ao final.' id='senha' name='senha' value=''>";
+
+            echo "<br><label for='campo_zona'>Local de moradia: </label><br>";
+                echo "<input type='radio' name='campo_zona' id='campo_zR' value=''>";
+            echo "<label for='campo_zR'>Zona rural</label>";
+                echo "<input type='radio' name='campo_zona' id='campo_zU' value=''>";
+            echo "<label for='campo_zU'>Zona urbana</label> <br><br>";
+
+            echo "<br><label for='tipo_empregado'>Tipo de empregado: </label><br>";
+                echo "<input type='radio' name='tipo_empregado' id='designado' value=''>";
+            echo "<label for='designado'>Designado</label>";
+                echo "<input type='radio' name='tipo_empregado' id='efetivo' value=''>";
+            echo "<label for='efetivo'>Efetivo</label> <br><br>";
+
+            echo "<br><label for='campo_s'>Sexo: </label><br>";
+                echo "<input type='radio' name='campo_s' id='campo_sM' value=''>";
+            echo "<label for='campo_sM'>Masculino</label>";
+                echo "<input type='radio' name='campo_s' id='campo_sF' value=''>";
+            echo "<label for='campo_sF'>Feminino</label> <br><br>";
+
+            echo "<br><label for='funcao' class='form-label'>Função: </label>";
+                echo "<input type='text' required class='form-control' id='funcao' name='funcao' value=''>";
+
+            echo "<br><label for='senha' class='form-label'>Senha: </label>";
+                echo "<input type='password' class='form-control' maxlength = '6' required name='senha' value=''>";
+
+            echo "<br><label for='confirm_senha' class='form-label'>Confirme a senha: </label>";
+                echo "<input type='password' class='form-control' maxlength = '6' required name='confirm_senha' value=''>";
+            
+            echo "<input class='btn btn-primary' type='submit' name='botao' value='Confirmar'>";
+        echo "</form>";
     }
 
     $formType = $_GET['id'];
@@ -78,21 +219,19 @@ if (!isset($_GET['id'])){
             def();
             break;
         case "supervisor":
-            echo "<h1 class='titulo_medio'>Cadastro de Supervisores</h1>";
-            sup_sec_dir();
+            supervisor();
             break;
         case "secretario":
-            echo "<h1 class='titulo_medio'>Cadastro de Secretários</h1>";
-            sup_sec_dir();
+            secretario();
             break;
         case "diretor":
-            echo "<h1 class='titulo_medio'>Cadastro de Diretor</h1>";
-            sup_sec_dir();
+            diretor();
             break;
         case "professor":
-            echo "<h1 class='titulo_medio'>Cadastro de Professores</h1>";
-            sup_sec_dir();
-            break;    
+            professor();
+            break;
+        case "aluno":
+            break;
     }
 ?>
 
