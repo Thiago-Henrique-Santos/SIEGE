634ef2122c185c097d9f6bf04267404326a7fa38
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-05-23 13:36:50 -0300

    Adição de um br e de um asterisco antes de alertar os erros

--- Validacoes/validacao_cadastrar.php
+++ Validacoes/validacao_cadastrar.php
@@ -36,52 +36,52 @@
 
     function validacao_sec_sup_prof_dir(){
         if (!isset($_POST['nome_completo'])) {
-            $msgErro_sec_sup_prof_dir["nome_completo"] = "Você esqueceu de inserir o nome!";
+            $msgErro_sec_sup_prof_dir["nome_completo"] = "<br> * Você esqueceu de inserir o nome!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['masp'])) {
-            $msgErro_sec_sup_prof_dir["masp"] = "Você esqueceu de inserir o número do MASP!";
+            $msgErro_sec_sup_prof_dir["masp"] = "<br> * Você esqueceu de inserir o número do MASP!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['email'])) {
-            $msgErro_sec_sup_prof_dir["email"] = "Você esqueceu de inserir o email!";
+            $msgErro_sec_sup_prof_dir["email"] = "<br> * Você esqueceu de inserir o email!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['campo_zona'])) {
-            $msgErro_sec_sup_prof_dir["campo_zona"] = "Você esqueceu de escolher a zona de residência!";
+            $msgErro_sec_sup_prof_dir["campo_zona"] = "<br> * Você esqueceu de escolher a zona de residência!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['tipo_empregado'])) {
-            $msgErro_sec_sup_prof_dir["tipo_empregado"] = "Você esqueceu de escolher o tipo de empregado!";
+            $msgErro_sec_sup_prof_dir["tipo_empregado"] = "<br> * Você esqueceu de escolher o tipo de empregado!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['campo_s'])) {
-            $msgErro_sec_sup_prof_dir["campo_s"] = "Você esqueceu de escolher o sexo!";
+            $msgErro_sec_sup_prof_dir["campo_s"] = "<br> * Você esqueceu de escolher o sexo!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['funcao'])) {
-            $msgErro_sec_sup_prof_dir["funcao"] = "Você esqueceu de inserir a função dessa pessoa!";
+            $msgErro_sec_sup_prof_dir["funcao"] = "<br> * Você esqueceu de inserir a função dessa pessoa!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['senha'])) {
-            $msgErro_sec_sup_prof_dir["senha"] = "Você esqueceu de inserir a senha de acesso!";
+            $msgErro_sec_sup_prof_dir["senha"] = "<br> * Você esqueceu de inserir a senha de acesso!";
             $cadastroCorreto = false;
         }
 
         if (!isset($_POST['confirm_senha'])) {
-            $msgErro_sec_sup_prof_dir["confirm_senha"] = "Você esqueceu de confirmar a senha de acesso!";
+            $msgErro_sec_sup_prof_dir["confirm_senha"] = "<br> * Você esqueceu de confirmar a senha de acesso!";
             $cadastroCorreto = false;
         }
 
         if ($_POST['senha'] != $_POST['confirm_senha']) {
-            $msgErro_sec_sup_prof_dir["confirmacao_senha"] = "As senhas não coincidem!";
+            $msgErro_sec_sup_prof_dir["confirmacao_senha"] = "<br> * As senhas não coincidem!";
             $cadastroCorreto = false;
         }
     }
@@ -90,57 +90,57 @@
     switch ($_POST['cargo']) {
         case "aluno":
             if (!isset($_POST['nome_completo'])) {
-                $msgErro_aluno["nome_completo"] = "Você esqueceu de inserir o nome do aluno!";
+                $msgErro_aluno["nome_completo"] = "<br> * Você esqueceu de inserir o nome do aluno!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['data_nascimento'])) {
-                $msgErro_aluno["data_nascimento"] = "Você esqueceu de inserir a data de nascimento!";
+                $msgErro_aluno["data_nascimento"] = "<br> * Você esqueceu de inserir a data de nascimento!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['matricula'])) {
-                $msgErro_aluno["matricula"] = "Você esqueceu de inserir o número da matricula!";
+                $msgErro_aluno["matricula"] = "<br> * Você esqueceu de inserir o número da matricula!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['responsavel'])) {
-                $msgErro_aluno["responsavel"] = "Você esqueceu de inserir o nome do responsável pelo aluno!";
+                $msgErro_aluno["responsavel"] = "<br> * Você esqueceu de inserir o nome do responsável pelo aluno!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['email'])) {
-                $msgErro_aluno["email"] = "Você esqueceu de inserir o email!";
+                $msgErro_aluno["email"] = "<br> * Você esqueceu de inserir o email!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['telefone'])) {
-                $msgErro_aluno["telefone"] = "Você esqueceu de inserir o telefone do responsável!";
+                $msgErro_aluno["telefone"] = "<br> * Você esqueceu de inserir o telefone do responsável!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['campo_zona'])) {
-                $msgErro_aluno["campo_zona"] = "Você esqueceu de escolher a zona em que o aluno mora!";
+                $msgErro_aluno["campo_zona"] = "<br> * Você esqueceu de escolher a zona em que o aluno mora!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['campo_s'])) {
-                $msgErro_aluno["campo_s"] = "Você esqueceu de escolher o sexo do aluno!";
+                $msgErro_aluno["campo_s"] = "<br> * Você esqueceu de escolher o sexo do aluno!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['senha'])) {
-                $msgErro_aluno["senha"] = "Você esqueceu de inserir a senha de acesso do aluno!";
+                $msgErro_aluno["senha"] = "<br> * Você esqueceu de inserir a senha de acesso do aluno!";
                 $cadastroCorreto = false;
             }
 
             if (!isset($_POST['confirm_senha'])) {
-                $msgErro_aluno["confirm_senha"] = "Você esqueceu de confirmar a senha de acesso do aluno!";
+                $msgErro_aluno["confirm_senha"] = "<br> * Você esqueceu de confirmar a senha de acesso do aluno!";
                 $cadastroCorreto = false;
             }
 
             if ($_POST['senha'] != $_POST['confirm_senha']) {
-                $msgErro_aluno["confirmacao_senha"] = "As senhas não coincidem!";
+                $msgErro_aluno["confirmacao_senha"] = "<br> * As senhas não coincidem!";
                 $cadastroCorreto = false;
             }
             break;
