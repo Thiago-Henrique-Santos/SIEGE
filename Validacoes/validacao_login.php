f4fa91eebe88eb6fc9240a3093d1fc5380f8e011
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-06-08 18:18:42 -0300

    Exclusão da animação de sucesso pós login e exclusão do css da mesma

--- Validacoes/validacao_login.php
+++ Validacoes/validacao_login.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-<META charset="utf-8">
-</head>
-<link rel="stylesheet" type="text/css" href="CSS/animacao_sucesso_login.css">
-<link rel="stylesheet" href="all.min.css">
-<body>
-
 <?php
     $erros_email="";
     $erros_senha="";
@@ -46,35 +36,9 @@
 
 
     if(strlen($erros_email)==0 and strlen($erros_senha)==0){
-        echo "<div class='success hide'>";
-            echo "<span class='check'><i class='fa fa-check-circle'></i></span>";
-            echo "<span class='msg'>Você foi logado com sucesso!></span>";
-            echo "<span class='crose'><i class='fa fa-times'></i></span>";
-        echo "</div>";
-
-        header("refresh:3;url=../visao_diretor.php");
+        header("Location: ../visao_diretor.php");
     }else{
         $valor = $_POST["campo_email"];
         header("Location: ../login.php?erros_email=$erros_email&erros_senha=$erros_senha&valor_email=$valor");
     }
 ?>
-
-<script>
-    $('button').click(function(){
-        $('.success').addClass("show");
-        $('.success').addClass("alert");
-        $('.success').removeClass("hide");
-        setTimeout(function(){
-            $('.success').removeClass("show");
-            $('.success').addClass("hide");
-        },3000);
-    });
-
-    $('.crose').click(function(){
-        $('.success').removeClass("show");
-        $('.success').addClass("hide");
-    });
-</script>
-
-</body>
-</html>
