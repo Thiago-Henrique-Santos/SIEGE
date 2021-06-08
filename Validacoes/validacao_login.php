2834bf23a082eac27f969e1aef799d6bf3a552a9
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-06-07 21:20:00 -0300

    Colocação do código da animação de sucesso pós login, porém ainda nao esta funcionando, faltam ajustes

--- Validacoes/validacao_login.php
+++ Validacoes/validacao_login.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<META charset="utf-8">
+</head>
+<link rel="stylesheet" type="text/css" href="CSS/animacao_sucesso_login.css">
+<link rel="stylesheet" href="all.min.css">
+<body>
+
 <?php
     $erros_email="";
     $erros_senha="";
@@ -36,8 +46,11 @@
 
 
     if(strlen($erros_email)==0 and strlen($erros_senha)==0){
-        echo "Login realizado com sucesso! <br>";
-        echo "Você será redirecionado para o sistema. <br>";
+        echo "<div class='success hide'>";
+            echo "<span class='check'><i class='fa fa-check-circle'></i></span>";
+            echo "<span class='msg'>Você foi logado com sucesso!></span>";
+            echo "<span class='crose'><i class='fa fa-times'></i></span>";
+        echo "</div>";
 
         header("refresh:3;url=../visao_diretor.php");
     }else{
@@ -45,3 +58,23 @@
         header("Location: ../login.php?erros_email=$erros_email&erros_senha=$erros_senha&valor_email=$valor");
     }
 ?>
+
+<script>
+    $('button').click(function(){
+        $('.success').addClass("show");
+        $('.success').addClass("alert");
+        $('.success').removeClass("hide");
+        setTimeout(function(){
+            $('.success').removeClass("show");
+            $('.success').addClass("hide");
+        },3000);
+    });
+
+    $('.crose').click(function(){
+        $('.success').removeClass("show");
+        $('.success').addClass("hide");
+    });
+</script>
+
+</body>
+</html>
