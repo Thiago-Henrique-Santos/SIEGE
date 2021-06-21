fb0b6ac988b468898209fb516238bebb81433f99
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-06-21 18:29:13 -0300

    Criação da página como acessar

---
+++ como_acessar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIEGE - Como acessar</title>
+    <link rel="stylesheet" type="text/css" href="CSS/reset.css">
+    <link rel="stylesheet" type="text/css" href="CSS/texto.css">
+    <link rel="stylesheet" type="text/css" href="CSS/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <div class="flex-container">
+        <?php
+            include ('componentes/first_nav.php');
+        ?>
+
+        <main>
+            <div style="width: 100%; height: 50px; display: flex; 
+            flex-flow: row nowrap; justify-content: center;">
+                <h1 class="titulo-principal"> Como acessar </h1>
+            </div>
+            
+            <p style="margin-top: 50px;">
+                Nesta página, iremos mostrar de forma bem explicativa como os pais podem acessar as notas do filho e a usar o sistema.
+            </p>
+        </main>
+    </div>
+
+
+</body>
+
+</html>
