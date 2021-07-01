03524b7501db7d72c264994cefd63d12a4b89287
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-07-01 15:08:05 -0300

    Create de discipina concluido

--- CRUD/Disciplina/create.php
+++ CRUD/Disciplina/create.php
@@ -2,11 +2,22 @@
 
 include ("../../BancoDados/conexao_mysql.php");
 
-$nome      = $_GET['nm'];
-$ano       = $_GET['ano'];
+$nome = $_GET['nm'];
+$ano = $_GET['ano'];
 $professor = $_GET['prf'];
-$turmas    = $_GET['tur'];
-var_dump($turmas);
+$turmas = $_GET['tur'];
+$idTurma = 0;
+
+    $prepara = $conexao->prepare("INSERT INTO disciplina (nome, ano, id_professor, id_turma) VALUES(?, ?, ?, ?)");
+    $prepara->bind_param("sdii", $nome, $ano, $professor, $idTurma);
+
+    foreach($turmas as $id_turma){
+        $idTurma = $id_turma;
+        $prepara->execute();
+    }
+
+    $prepara->close();
+    header ("Location: ../../formularios-cadastro.php?id=validadoOK");
 
 $conexao->close();
 
