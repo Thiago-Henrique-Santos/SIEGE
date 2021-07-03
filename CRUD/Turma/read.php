9a91048e8f0baf078736caeba16a2873b1fca667
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-07-03 20:40:52 -0300

    Começo do read para turmas

--- CRUD/Turma/read.php
+++ CRUD/Turma/read.php
@@ -2,7 +2,12 @@
 
     include ("C:/xampp/htdocs/SIEGE/BancoDados/conexao_mysql.php");
 
-    $sql = "SELECT * FROM turma ORDER BY serie ASC";
+    $sql = "
+            SELECT t.serie AS 'serie', t.nome AS 'nome_turma', d.nome AS 'disciplina', u.id AS 'id_professor', 
+            u.nome AS 'professor' FROM turma t INNER JOIN disciplina d ON d.id_turma=t.id INNER JOIN usuario u 
+            ON u.id=d.id_professor ORDER BY serie ASC
+            ";
+            
 	$resultado = $conexao->query($sql);
 	
 	if ($resultado->num_rows > 0)
@@ -11,7 +16,8 @@
 		{
             echo "<div style='border: 1px solid black;'>";
 			echo "Turma: " . $linha['serie'] . "°" . " ano " . $linha['nome'];
-            
+
+
             echo "&nbsp; <button>Atualizar</button> &nbsp;";
             echo "<button>Remover</button>";
             echo "</div>";
