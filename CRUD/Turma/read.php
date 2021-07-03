495f7ce6eda3a1839d77eb76ada3fa219a78a745
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-07-03 18:47:22 -0300

    Read de turma

--- CRUD/Turma/read.php
+++ CRUD/Turma/read.php
@@ -1,8 +1,25 @@
 <?php
 
-include ("../../BancoDados/conexao_mysql.php");
+    include ("C:/xampp/htdocs/SIEGE/BancoDados/conexao_mysql.php");
 
+    $sql = "SELECT * FROM turma ORDER BY serie ASC";
+	$resultado = $conexao->query($sql);
+	
+	if ($resultado->num_rows > 0)
+	{
+		while ($linha = $resultado->fetch_assoc())
+		{
+            echo "<div style='border: 1px solid black;'>";
+			echo "Turma: " . $linha['serie'] . "°" . " ano " . $linha['nome'];
+            
+            echo "&nbsp; <button>Atualizar</button> &nbsp;";
+            echo "<button>Remover</button>";
+            echo "</div>";
+        }
+    }
+	else
+		echo "Não foram encontradas turmas!";	
 
-$conexao->close();
+    $conexao->close();
 
 ?>
