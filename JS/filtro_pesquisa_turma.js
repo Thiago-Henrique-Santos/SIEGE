935ad8636ac4dd3b86131779495595a0c446180f
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-07-06 20:32:28 -0300

    término de completar o arquivo filtro_pesquisa_turma, read_filtro e coloquei a tag script no arquivo turma.php

--- JS/filtro_pesquisa_turma.js
+++ JS/filtro_pesquisa_turma.js
@@ -1,3 +1,28 @@
-var ajax =  new XMLHttpRequest();
+var btn = document.getElementById("btn");
 
-ajax.open("GET", "../CRUD/Turma/read_filtro.php");
+btn.addEventListener("click", function(){
+    var ajax =  new XMLHttpRequest();
+
+    ajax.open("GET", "../CRUD/Turma/read_filtro.php");
+
+    ajax.responseType = "json";
+
+    ajax.send();
+
+    ajax.addEventListener("readystatechange", function (){
+
+        if(ajax.readyState === 4 && ajax.status === 200){
+            
+            console.log(ajax);
+            console.log(ajax.response);
+            var resposta = ajax.response;
+            var lista = document.querySelector(".list");
+
+            for(var i = 0; i < resposta.length; i++){
+                lista.innerHTML += "<li>" + resposta[i] + "</li>";
+            }
+
+        }
+
+    });
+})
--- turmas.php
+++ turmas.php
@@ -14,6 +14,8 @@
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+     <script src="JS/filtro_pesquisa_turma.js" type="module" defer></script>   
 </head>
 
 <body>
@@ -33,28 +35,28 @@
     <form class="user" action="#" method="post">
         <label>Marque o ano da turma que gostaria de ver os registros:</label> <br><br>
 
-        <input type="checkbox" class="anoFiltro" id="seg" name="seg" value="segundo">
+        <input type="checkbox" class="serieFiltro" id="seg" name="seg" value="segundo">
         <label for="seg">2° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="terc" name="terc" value="terceiro">
+        <input type="checkbox" class="serieFiltro" id="terc" name="terc" value="terceiro">
         <label for="sup">3° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="quart" name="quart" value="quarto">
+        <input type="checkbox" class="serieFiltro" id="quart" name="quart" value="quarto">
         <label for="quart">4° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="quin" name="quin" value="quinto">
+        <input type="checkbox" class="serieFiltro" id="quin" name="quin" value="quinto">
         <label for="quin">5° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="sext" name="sext" value="sexto">
+        <input type="checkbox" class="serieFiltro" id="sext" name="sext" value="sexto">
         <label for="sext">6° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="seti" name="seti" value="setimo">
+        <input type="checkbox" class="serieFiltro" id="seti" name="seti" value="setimo">
         <label for="seti">7° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="oit" name="oit" value="oitavo">
+        <input type="checkbox" class="serieFiltro" id="oit" name="oit" value="oitavo">
         <label for="oit">8° anos</label>
 
-        <input type="checkbox" class="anoFiltro" id="non" name="non" value="nono">
+        <input type="checkbox" class="serieFiltro" id="non" name="non" value="nono">
         <label for="non">9° anos</label><br>
     </form>
     <br><br>
