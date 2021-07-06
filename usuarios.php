c219da83b3e62787d4c5f1fbf3c4ef582f7b8b56
Thiago Santos <thilego418@gmail.com>
2021-07-06 14:46:37 -0300

    Adicionando classe cargoFiltro aos checkboxes da página usuarios.php

--- usuarios.php
+++ usuarios.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" type="text/css" href="CSS/forms.css">
     <link rel="stylesheet" type="text/css" href="CSS/texto.css">
     <link rel="stylesheet" type="text/css" href="CSS/user_main.css">
-    <script src="JS/filtro_pesquisa_usuario.js" async></script>
+    <script src="JS/filtro_pesquisa_usuario.js" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
