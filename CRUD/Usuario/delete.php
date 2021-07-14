af5b9208ac600f1be7e56211fb00c56f94063966
Ana Eliza Feitoza <ana.eliza@alunos.ifsuldeminas.edu.br>
2021-07-14 15:07:43 -0300

    deletar teste

--- CRUD/Usuario/delete.php
+++ CRUD/Usuario/delete.php
@@ -1,10 +1,16 @@
 <?php
 
 include ("../../BancoDados/conexao_mysql.php");
+    $usu_codigo = intval($_GET['usuario']);
+    $sql_code = "DELETE FROM usuario WHERE codigo = '$usu_codigo'";
+    $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
 
-
-
-
+    if($sql_query)
+    echo <script> location.href='index.php?p=inicial'; </script>
+    else 
+    echo <script> alert('Usuário não foi deletado.');
+    location.href='index.php?p=inicial'; </script>
 $conexao->close();
 
 ?>
+
