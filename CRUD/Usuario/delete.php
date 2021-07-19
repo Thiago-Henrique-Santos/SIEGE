fa2105857380e8cebbc1da57b17a09a2b9468e56
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-07-19 16:47:13 -0300

    adicionei o if de id para o delete.php de usuario

--- CRUD/Usuario/delete.php
+++ CRUD/Usuario/delete.php
@@ -1,7 +1,10 @@
 <?php
 
-include ("../../BancoDados/conexao_mysql.php");
+if(!isset($_GET['id']) || empty($_GET['id'])){
+    echo "Ocorreu um erro!";
+}
 
+include ("../../BancoDados/conexao_mysql.php");
 
 
 
