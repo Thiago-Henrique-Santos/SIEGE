31300fd44c5b4604de863d2a68d3b66b506aef44
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-07-26 17:33:04 -0300

    Comecei a página de ferramentas

--- ferramentas.php
+++ ferramentas.php
@@ -5,10 +5,11 @@
     <META charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIEGE - Informações para Diretor e Vice-Diretor </title>
+    <title>SIEGE - Ferramentas</title>
     <link rel="stylesheet" type="text/css" href="CSS/reset.css">
     <link rel="stylesheet" type="text/css" href="CSS/texto.css">
     <link rel="stylesheet" type="text/css" href="CSS/user_main.css">
+    <link rel="stylesheet" type="text/css" href="CSS/cadastrar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
@@ -20,9 +21,23 @@
     ?>
 
     <h1 class="titulo-principal"> Ferramentas </h1>
+    <br>
 
-    <p style="margin-top: 25px;"> Nesta página irá conter as ferramentas que os professores usam
-    como o google sala de aula, google jamboard e google meet </p>
+    <div class="buttons-group" style="margin-top:25px;">
+        <button href="https://classroom.google.com/" target="area">Google Classroom</button>
+        <button href="https://jamboard.google.com/" target="area">Google Jamboard</button>
+        <button href="https://meet.google.com/" target="area">Google Meet</button>
+        <button href="https://mail.google.com/" target="area">Gmail</button>
+    </div>
+
+    <div class="buttons-group" style="margin-top:25px;">
+        <button href="https://simave.educacao.mg.gov.br/#!/pagina-inicial" target="area">Simave</button>
+        <button href="https://www2.educacao.mg.gov.br/" target="area">Secretaria Educação MG</button>
+    </div>
+
+    <center>
+	<iframe name="area" src="https://www.extreme-e.com/" height="750px" width="98%" style="margin-top: 30px;"></iframe>
+    </center>
 </body>
 
 </html>
