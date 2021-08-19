4478219e72a3abb80046311c5b815c2ec7406ad2
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-08-19 16:47:41 -0300

    Ja criamos o json no read2, para a pagina turmas2.php

---
+++ JS/filtro_pesquisa_turma2.js
@@ -0,0 +1,23 @@
+import { classAsyncQuery } from '../modulos/filtros.js';
+
+var options = document.getElementsByClassName('serieFiltro');
+var resultBlock = document.getElementById('busca_resultado');
+var url = "CRUD/Turma/read2.php";
+
+for (let i = 0; i < options.length; i++) {
+    let checkbox = options[i];
+    checkbox.addEventListener('change', function (event) {
+        var checkboxStatus = event.target.checked;
+        if (checkboxStatus) {
+            classAsyncQuery(url, resultBlock, checkbox.id, checkboxStatus);
+        }
+    });
+}
+
+window.onload = () => {
+    sessionStorage.setItem('path', url);
+    sessionStorage.setItem('relativeCounter', 0);
+    sessionStorage.setItem('relativeStart', false);
+    sessionStorage.setItem('firstRelative', "");
+    classAsyncQuery(url, resultBlock, null, null);
+}
