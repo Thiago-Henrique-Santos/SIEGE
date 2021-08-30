af86124ab794c735333faecc884e6d325bbfccad
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-08-30 18:25:57 -0300

    Tirei a opção turmas 2 do user_nav

--- componentes/user_nav.php
+++ componentes/user_nav.php
@@ -9,7 +9,6 @@
         echo "<li> <a href='ferramentas.php'>Ferramentas</a> </li>";
         echo "<li> <a href='usuarios.php'>Registros</a> </li>";
         echo "<li> <a href='turmas.php'>Turmas</a> </li>";
-        echo "<li> <a href='turmas2.php'>Turmas2</a> </li>";
         echo "<li> <a href='index.php'>Sair</a> </li>";
     echo "</ul>";
     echo "</nav>";
