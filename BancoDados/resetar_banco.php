5faadeb312a1493614b9ae21283d5c31c2c7cfef
Thiago Henrique dos Santos <72514176+Thiago-Henrique-Santos@users.noreply.github.com>
2021-09-02 10:38:33 -0300

    Criando arquivo para resetar o banco - executar os arquivos de excluir banco, criar banco, criar tabelas e inserir dados automaticamente

---
+++ BancoDados/resetar_banco.php
@@ -0,0 +1,10 @@
+<?php
+
+include ('excluir_banco.php');
+include('criar_banco.php');
+include('criar_tabelas.php');
+include('inserir_dados.php');
+
+echo "<br><br><font color='limegreen'>Banco de dados resetado com sucesso!</font>"
+
+?>
