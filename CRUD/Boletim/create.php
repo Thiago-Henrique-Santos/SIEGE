4a38623c63670b7a9e27344f297e3810ab0f2513
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-09-06 11:28:31 -0300

    Alterei o arquivo create.php do CRUD de Boletim

--- CRUD/Boletim/create.php
+++ CRUD/Boletim/create.php
@@ -2,6 +2,15 @@
 
 include ("../../BancoDados/conexao_mysql.php");
 
+$id_aluno = $_GET['id_aluno'];
+$id_disciplina = $_GET['id_disciplina'];
+
+$sql = "INSERT INTO boletim (id_aluno, id_disciplina) VALUES ($id_aluno, $id_disciplina)";
+
+	if ($conexao->query($sql) === TRUE)
+		echo "Linha criada com sucesso";
+	else
+		echo "Erro inserindo linha: " . $conexao->error;
 
 $conexao->close();
 
