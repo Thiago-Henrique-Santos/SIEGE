3538c52585dc85595854d5fea683f77301ea37b0
Thiago-Henrique-Santos <thilego418@gmail.com>
2021-09-08 14:26:53 -0300

    Atualizado create de boletim com algumas mecânicas novas necessárias

--- CRUD/Boletim/create.php
+++ CRUD/Boletim/create.php
@@ -2,13 +2,19 @@
 
 include ("../../BancoDados/conexao_mysql.php");
 
-$id_aluno = $_GET['idtf'];
-$id_disciplina = $_GET['idd'];
+$registeredEntity = $_GET['ent'];
+$id_aluno         = $_GET['ida'];
+$id_disciplina    = $_GET['idd'];
 
 $prepara = $conexao->prepare("INSERT INTO boletim (id_aluno, id_disciplina) VALUES (?, ?)");
 $prepara->bind_param("ii", $id_aluno, $id_disciplina);
 $prepara->execute();
 $prepara->close();
+
+if ($registeredEntity == "disciplina")
+    header ("Location: ../../formularios-cadastro.php?id=validadoOK&tfm=cadastrar&rcd=disciplina");
+else
+    header ("Location: ../../formularios-cadastro.php?id=validadoOK&tfm=cadastrar&rcd=aluno");
 
 $conexao->close();
 
