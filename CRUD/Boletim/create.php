10a20ef0f96e19a2687b13f92efb41e1d9628f35
Thiago-Henrique-Santos <thilego418@gmail.com>
2021-09-09 15:59:35 -0300

    Concluído create de boletim e funcinamento do create quando se insere disciplina

--- CRUD/Boletim/create.php
+++ CRUD/Boletim/create.php
@@ -1,14 +1,36 @@
 <?php
 
 include ("../../BancoDados/conexao_mysql.php");
+if (isset($_GET['ent']))
+    $registeredEntity = $_GET['ent'];
 
-$registeredEntity = $_GET['ent'];
-$id_aluno         = $_GET['ida'];
-$id_disciplina    = $_GET['idd'];
+if (isset($_GET['ida']))
+    $id_aluno = $_GET['ida'];
+
+if (isset($_GET['idd']))
+    $id_disciplina = $_GET['idd'];
 
 $prepara = $conexao->prepare("INSERT INTO boletim (id_aluno, id_disciplina) VALUES (?, ?)");
 $prepara->bind_param("ii", $id_aluno, $id_disciplina);
-$prepara->execute();
+
+if (isset($_GET['qta'])) {
+    $quantidadeAlunos = $_GET['qta'];
+    for ($i = 0; $i < $quantidadeAlunos; $i++) {
+        $getName = "ida$i";
+        $id_aluno = $_GET[$getName];
+        $prepara->execute();
+    }
+}
+
+if (isset($_GET['qtd'])) {
+    $quantidadeDisciplinas = $_GET['qtd'];
+    for ($i = 0; $i < $quantidadeDisciplinas; $i++) {
+        $getName = "idd$i";
+        $id_aluno = $_GET[$getName];
+        $prepara->execute();
+    }
+}
+
 $prepara->close();
 
 if ($registeredEntity == "disciplina")
