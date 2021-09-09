0880dc317a01d88484de6ecad396cf23719393a5
Thiago-Henrique-Santos <thilego418@gmail.com>
2021-09-09 15:42:39 -0300

    Sistema de verificação se já existe aluno na turma em que uma disciplina está sendo inserida concluído

--- CRUD/Disciplina/create.php
+++ CRUD/Disciplina/create.php
@@ -8,16 +8,42 @@ $professor = $_GET['prf'];
 $turmas = $_GET['tur'];
 $idTurma = 0;
 
-    $prepara = $conexao->prepare("INSERT INTO disciplina (nome, ano, id_professor, id_turma) VALUES(?, ?, ?, ?)");
-    $prepara->bind_param("sdii", $nome, $ano, $professor, $idTurma);
+$prepara = $conexao->prepare("INSERT INTO disciplina (nome, ano, id_professor, id_turma) VALUES(?, ?, ?, ?)");
+$prepara->bind_param("sdii", $nome, $ano, $professor, $idTurma);
 
-    foreach($turmas as $id_turma){
-        $idTurma = $id_turma;
-        $prepara->execute();
+$idsAlunos = array();
+$quantidadeAlunos = 0;
+foreach($turmas as $id_turma){
+    $idTurma = $id_turma;
+    $prepara->execute();
+    
+    $sql       = "SELECT idAluno FROM aluno WHERE id_turma = $idTurma";
+    $resultado = $conexao->query($sql);
+    if ($resultado->num_rows > 0) {
+        while ($dados = $resultado->fetch_assoc()) {
+            $idAluno      = $dados['idAluno'];
+            array_push($idsAlunos, $idAluno);
+            $quantidadeAlunos++;
+        }
     }
+}
 
-    $prepara->close();
-    header ("Location: ../../formularios-cadastro.php?id=validadoOK&tfm=cadastrar&rcd=disciplina");
+$prepara->close();
+
+$idDisciplina = 0;
+$sql          = "SELECT id FROM disciplina WHERE nome = '$nome' AND ano = $ano AND id_turma = $idTurma";
+$resultado2   = $conexao->query($sql);
+if ($resultado2->num_rows > 0) {
+    while ($dados2 = $resultado2->fetch_assoc()) {
+        $idDisciplina = $dados2['id'];
+    }
+}
+
+$headerLink = "Location: ../Boletim/create.php?ent=disciplina&idd=$idDisciplina&qta=$quantidadeAlunos";
+for ($i=0; $i < $quantidadeAlunos; $i++) {
+    $headerLink .= "&ida$i=$idsAlunos[$i]";
+}
+header ($headerLink);
 
 $conexao->close();
 
