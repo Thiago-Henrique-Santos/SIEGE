96bac2ac4921058b22d69f0d41338eeb51abc664
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-09-10 12:05:08 -0300

    Foi começado o UPDATE de boletim, e tirei um echo no update de disciplina

--- CRUD/Boletim/update.php
+++ CRUD/Boletim/update.php
@@ -2,7 +2,18 @@
 
 include ("../../BancoDados/conexao_mysql.php");
 
+$idTurma      = $_GET['tur'];
+$idDisciplina = $_GET['dsc'];
 
+        $sql2 = " UPDATE boletim SET id = $dados['id_boletim'], 
+                  nota1bim = $dados['nota1'], nota2bim = $dados['nota2'], nota3bim = $dados['nota3'], nota4bim = $dados['nota4'],  
+                  falta1bim = $dados['falta1'], falta2bim = $dados['falta2'], falta3bim = $dados['falta3'], falta4bim = $dados['falta4'] 
+                  WHERE id=$id AND id_disciplina = $idDisciplina AND id_turma = $idTurma";
+                  
+        if ($conexao->query($sql2) === TRUE)
+            header("Location: ../../formularios-cadastro.php?id=validadoOK&tfm=atualizar");
+        else
+            header("location: ../../formularios-cadastro.php?id=erro&tfm=atualizar&info=" . $conexao->error);
 
 $conexao->close();
 
