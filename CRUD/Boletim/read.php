81e58092c8afe0e6f6a65ae6e120a1effb95994a
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-09-10 13:43:55 -0300

    Modificação no read de boletim, para aparecer a nota e falta de recuperacao

--- CRUD/Boletim/read.php
+++ CRUD/Boletim/read.php
@@ -8,7 +8,7 @@ $idDisciplina = $_GET['dsc'];
 $registros = array();
 $sql = "SELECT b.nota1bim AS 'nota1', b.nota2bim AS 'nota2', b.nota3bim AS 'nota3', b.nota4bim AS 'nota4', 
         b.falta1bim AS 'falta1', b.falta2bim AS 'falta2', b.falta3bim AS 'falta3', b.falta4bim AS 'falta4', 
-        b.id AS 'id_boletim', u.nome AS 'aluno' FROM boletim b INNER JOIN usuario u ON b.id_aluno = u.id INNER JOIN aluno a ON a.idAluno = u.id 
+        b.notaRecuperacao AS 'nota_r', b.faltaRecuperacao AS 'falta_r', b.id AS 'id_boletim', u.nome AS 'aluno' FROM boletim b INNER JOIN usuario u ON b.id_aluno = u.id INNER JOIN aluno a ON a.idAluno = u.id 
         WHERE b.id_disciplina = $idDisciplina AND a.id_turma = $idTurma";
 $resultado = $conexao->query($sql);
 if ($resultado->num_rows > 0) {
@@ -18,20 +18,22 @@ if ($resultado->num_rows > 0) {
             1 => $dados['nota1'],
             2 => $dados['nota2'],
             3 => $dados['nota3'],
-            4 => $dados['nota4']
+            4 => $dados['nota4'],
+            5 => $dados['nota_r']
         );
         $faltas = array(
             1 => $dados['falta1'],
             2 => $dados['falta2'],
             3 => $dados['falta3'],
-            4 => $dados['falta4']
+            4 => $dados['falta4'],
+            5 => $dados['falta_r']
         );
         $aluno   = $dados['aluno'];
         $boletim = $dados['id_boletim'];
 
         $registros[$i]['id_boletim'] = $boletim;
         $registros[$i]['aluno'] = $aluno;
-        for ($j=1; $j < 5; $j++) { 
+        for ($j=1; $j < 6; $j++) { 
             $registros[$i]['notas'][$j] = $notas[$j];
             $registros[$i]['faltas'][$j] = $faltas[$j];
         }
