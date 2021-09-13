ff29179d5c8832707a010d6839690a39d72fa676
Thiago-Henrique-Santos <thilego418@gmail.com>
2021-09-13 16:09:30 -0300

    Passando de sql query para prepare no update de boletim

--- CRUD/Boletim/update.php
+++ CRUD/Boletim/update.php
@@ -5,40 +5,87 @@ include ("../../BancoDados/conexao_mysql.php");
 if (isset($_GET['qtd'])) {
     $quantidade = $_GET['qtd'];
 
+    $nota1 = 0; $faltas1 = 0;
+    $nota2 = 0; $faltas2 = 0;
+    $nota3 = 0; $faltas3 = 0;
+    $nota4 = 0; $faltas4 = 0;
+    $notaRecuperacao   = 0;
+    $faltasRecuperacao = 0;
+    $idBoletim         = 0;
+
+    $preparaSQL = "UPDATE boletim SET nota1bim = ?, nota2bim = ?, nota3bim = ?, nota4bim = ?, notaRecuperacao = ?, 
+    falta1bim = ?, falta2bim = ?, falta3bim = ?, falta4bim = ?, faltaRecuperacao = ? WHERE id = ?";
+    $prepara = $conexao->prepare($preparaSQL);
+    $prepara->bind_param("dddddiiiiii", $nota1, $nota2, $nota3, $nota4, $notaRecuperacao, $faltas1, $faltas2, $faltas3, $faltas4, $faltasRecuperacao, $idBoletim);
+    
     for ($i = 0; $i < $quantidade; $i++) {
-        $getName   = "idb$i";
+        $getName = "idb$i";
         $idBoletim = $_GET[$getName];
 
-        $getName  = "n1$i";
-        $nota1    = $_GET[$getName];
-        $getName  = "n2$i";
-        $nota2    = $_GET[$getName];
-        $getName  = "n3$i";
-        $nota3    = $_GET[$getName];
-        $getName  = "n4$i";
-        $nota4    = $_GET[$getName];
-
-        $getName    = "f1$i";
-        $faltas1    = $_GET[$getName];
-        $getName    = "f2$i";
-        $faltas2    = $_GET[$getName];
-        $getName    = "f3$i";
-        $faltas3    = $_GET[$getName];
-        $getName    = "f4$i";
-        $faltas4    = $_GET[$getName];
-        
-        $getName           = "rn$i";
-        $notaRecuperacao   = $_GET[$getName];
-        $getName           = "rf$i";
-        $faltasRecuperacao = $_GET[$getName];
-        
-        $sql = "UPDATE boletim SET nota1bim = $nota1, nota2bim = $nota2, nota3bim = $nota3 , nota4bim = $nota4, 
-        falta1bim = $faltas1, falta2bim = $faltas2, falta3bim = $faltas3, falta4bim = $faltas4
-        WHERE id = $idBoletim";
-        if ($conexao->query($sql))
-            echo "<font color='lime'>Deu certo! Substituir pela mensagem de sucesso de atualização!</font> <br>";
+        $getName = "n1$i";
+        if (!empty($_GET[$getName]))
+            $nota1 = $_GET[$getName];
         else
-            echo $conexao->error . "<font color='red'> - Substituir por mensagem de erro.</font>";
+            $nota1 = NULL;
+
+        $getName = "n2$i";
+        if (!empty($_GET[$getName]))
+            $nota2 = $_GET[$getName];
+        else
+            $nota2 = NULL;
+
+        $getName = "n3$i";
+        if (!empty($_GET[$getName]))
+            $nota3 = $_GET[$getName];
+        else
+            $nota3 = NULL;
+
+        $getName = "n4$i";
+        if (!empty($_GET[$getName]))
+            $nota4 = $_GET[$getName];
+        else
+            $nota4 = NULL;
+
+        $getName = "f1$i";
+        if (!empty($_GET[$getName]))
+            $faltas1    = $_GET[$getName];
+        else
+            $faltas1 = NULL;
+
+        $getName = "f2$i";
+        if (!empty($_GET[$getName]))
+            $faltas2    = $_GET[$getName];
+        else
+            $faltas2 = NULL;
+
+        $getName = "f3$i";
+        if (!empty($_GET[$getName]))
+            $faltas3 = $_GET[$getName];
+        else
+            $faltas3 = NULL;
+
+        $getName = "f4$i";
+        if (!empty($_GET[$getName]))
+            $faltas4 = $_GET[$getName];
+        else
+            $faltas4 = NULL;
+        
+        $getName = "rn$i";
+        if (!empty($_GET[$getName]))
+            $notaRecuperacao = $_GET[$getName];
+        else
+            $notaRecuperacao = NULL;
+
+        $getName = "rf$i";
+        if (!empty($_GET[$getName]))
+            $faltasRecuperacao = $_GET[$getName];
+        else
+            $faltasRecuperacao = NULL;
+            
+        if ($prepara->execute())
+            echo "1";
+        else
+            echo "0";
     }
 }
 
