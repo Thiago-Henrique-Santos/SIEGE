ad88e53cfb6fd35b73d2b5b361986eabe1df6e1d
Raphael Ferreira <raphaelkombate@gmail.com>
2021-10-15 16:58:14 -0300

    Fiz os combobox de turma e disciplina

--- JS/boletim.js
+++ JS/boletim.js
@@ -1,22 +1,94 @@
-function toogle_disabled(bool)
-{
+function toogle_disabled(bool) {
 	var input = document.getElementsByTagName('input');
+	var botao_publicar = document.getElementById('btn_publicar');
 
-	for(var i=0; i<=(input.length-1); i++ )
-	{
-		if( input[i].type=='number' ) 
+	for (var i = 0; i <= (input.length - 1); i++) {
+		if (input[i].type == 'number')
 			input[i].disabled = bool;
 	}
+
+	botao_publicar.disabled = false;
+	botao_publicar.style.backgroundColor = "rgb(85, 84, 84)";
+	botao_publicar.style.color = "white";
+	botao_publicar.style.cursor = "pointer";
+	botao_publicar.addEventListener("mouseover", function(event){
+		event.target.style.textDecoration = "underline";
+		event.target.style.color = "white";
+	})
+		botao_publicar.addEventListener("mouseout", function(event){
+		event.target.style.textDecoration = "none";
+		event.target.style.color = "white";
+	})
 }
 
 function cancel(bool) {
 	var input = document.getElementsByTagName('input');
+	var botao_publicar = document.getElementById('btn_publicar');
 
 	for (var i = 0; i <= (input.length - 1); i++) {
 		if (input[i].type == 'number') {
-			input[i].value = "";
 			input[i].disabled = bool;
 		}
 	}
+
+	botao_publicar.disabled = true;
+	botao_publicar.style.backgroundColor = "rgb(157, 157, 157)";
+	botao_publicar.style.color = "rgb(177, 177, 177)";
+	botao_publicar.style.cursor = "default";
+
 }
 
+function clearInputs() {
+	var input = document.getElementsByTagName('input');
+	var conf;
+	var confirmacao;
+
+	for (var i = 0; i <= (input.length - 1); i++) {
+		if (input[i].type == 'number' && !input[i].disabled) {
+				conf = true;
+		}
+	}
+
+	if(conf){
+		confirmacao = confirm ("Você realmente deseja limpar os dados de todos os campos?");
+		if(confirmacao == true)
+			for (var i = 0; i <= (input.length - 1); i++) {
+				if (input[i].type == 'number') {
+					input[i].value = "";
+				}
+			}	
+	}
+}
+
+function postGrades(){
+	var input = document.getElementsByTagName('input');
+
+	for (var i = 0; i <= (input.length - 1); i++) {
+		if (input[i].type == 'number') {
+			input[i].disabled = true;
+		}
+	}
+
+	var info = [];
+	var linhas = document.querySelectorAll('#boletim tr');
+	for(let i = 2; i < linhas.length; i++){
+		var linha = linhas[i];
+		var colunas = linha.getElementsByTagName('td');
+		var linha_info = [];
+		for(let j = 1; j < colunas.length-3; j++){
+			var coluna = colunas[j];
+			var valor = coluna.getElementsByTagName('input')[0].value;
+			linha_info.push(valor);
+		}
+		info.push(linha_info);
+	}
+	console.log(info);
+
+	var quantbole = info.length;
+	var url = `CRUD/Boletim/read.php?qtd=${quantbole}`;
+	for(let i = 0; i < quantbole; i++){
+		for(let j = 0; j < info[i].length; j++){
+			url+=`&idb${i}=${info[i][j]}&n1${i}=`;
+		}
+	} 
+}
--- boletim.php
+++ boletim.php
@@ -1,4 +1,5 @@
 <?php
+    include ('BancoDados/conexao_mysql.php');
     session_start();
     if(!isset($_SESSION['campo_email']) || empty($_SESSION['campo_email'])){
         header ("Location: login.php");
@@ -18,7 +19,7 @@
     <link rel="stylesheet" type="text/css" href="CSS/user_main.css">
     <link rel="stylesheet" href="CSS/boletim.css" type="text/css">
     <script src="JS/boletim.js"></script>
-    <script src="JS/filtro_boletim.js"></script>
+    <script src="JS/filtro_boletim.js" type="module"></script>
 </head>
 
 <body>
@@ -30,13 +31,46 @@
     <h1 class="titulo-principal">Boletim</h1>
     <button id="btn_editar" name="btn_editar" type="button" onclick="toogle_disabled(false)">Editar</button>
     <button id="btn_cancelar" name="btn_cancelar" type="button" onclick="cancel(true)">Cancelar</button>
+    <button id="btn_limpar" name="btn_limpar" type="button" onclick="clearInputs()">Limpar</button>
+    <button id="btn_publicar" name="btn_publicar" type="button" onclick="postGrades()" disabled="">Publicar</button>
 
     <br><br>
-    <!--tr = linha, td = coluna-->
-
-    <!-- Tabela na visão de gerenciadores e professores -->
     
-        <table class="boletim">
+                <select name="turmas">
+                    <option selected="sut">Selecione uma Turma</option>
+                    <?php
+                        $prepara = $conexao->prepare("SELECT * FROM turma WHERE id != 1");
+                        $prepara->execute();
+                        $resultado = $prepara->get_result();
+                        while($t = $resultado->fetch_object()){
+                            $turmas[] = $t;
+                        }
+                        foreach ($turmas as $tur){
+							echo "<option value = $tur->id>" . $tur->serie . "º " . $tur->nome . "</option>";
+						}
+                    ?>
+                </select>
+                &emsp;
+                <select name="disciplinas">
+                    <option selected="sud">Selecione uma Disciplina</option>
+                    <?php
+                        $prepara2 = $conexao->prepare("SELECT nome FROM disciplina WHERE id_turma = /*$(Variável com o id da turma selecionada)*/");
+                        $prepara2->execute();
+                        $resultado2 = $prepara2->get_result();
+                        while($d = $resultado2->fetch_object()){
+                            $disciplinas[] = $d;
+                        }
+                        foreach ($disciplinas as $dis){
+							echo "<option value = $dis->id>" . $dis->nome . "</option>";
+						}
+                    ?>
+                </select>
+                
+    <br><br>
+    
+    <!--tr = linha, td = coluna-->
+    <!-- Tabela na visão de gerenciadores e professores -->
+        <table id="boletim">
 
             <!-- ***** Parte Fixa abaixo*****-->
             <tr class="titulo_tabela">
@@ -57,16 +91,20 @@
                     <h2>4° Bimestre</h2>
                 </td>
 
-                <td rowspan="2">
-                    <h2>Faltas finais</h2>
+                <td colspan="2">
+                    <h2>Recuperação</h2>
                 </td>
 
                 <td rowspan="2">
-                    <h2>Notas finais</h2>
+                    <h2>Faltas totais</h2>
                 </td>
 
                 <td rowspan="2">
-                    <h2>Situações</h2>
+                    <h2>Nota Final</h2>
+                </td>
+
+                <td rowspan="2">
+                    <h2>Situação</h2>
                 </td>
             </tr>
 
@@ -111,9 +149,265 @@
                     <h2>Notas</h2>
                 </td>
 
+                <!--Recuperação-->
+                <td>
+                    <h2>Faltas</h2>
+                </td>
+
+                <td>
+                    <h2>Notas</h2>
+                </td>
+
             </tr>
 
-            
+            <tr class="linha_registros">
+
+                <!--Primeira coluna -> Aluno-->
+                <td class="nome">
+                    <h3>Ana Clara Souza</h3>
+                </td>
+
+                <!--Segunda coluna -> Falta 1° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f1b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Terceira coluna -> Nota 1° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n1b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Quarta coluna -> Falta 2° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f2b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Quinta coluna -> Nota 2° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n2b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Sexta coluna -> Falta 3° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f3b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Sétima coluna -> Nota 3° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n3b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Oitava coluna -> Falta 4° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f4b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Nona coluna -> Nota 4° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n4b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Décima coluna -> Falta recuperação-->
+                <td>
+                    <input min="0" type="number" step="1" name="f4b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Décima primeira coluna -> Nota recuperação-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n3b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Falta final-->
+                <td>
+                    <h3>
+                        <font color="green">10</font>
+                    </h3>
+                </td>
+
+                <!--Nota final-->
+                <td>
+                    <h3>
+                        <font color="green">100,0</font>
+                    </h3>
+                </td>
+
+                <!--Situação-->
+                <td>
+                    <h3>
+                        <font color="green">Aprovada</font>
+                    </h3>
+                </td>
+
+            </tr>
+
+
+            <!-- Registro 2 -->
+            <tr class="linha_registros">
+
+                <!--Primeira coluna -> Aluno-->
+                <td class="nome">
+                    <h3>Bianca Barroso da Silva</h3>
+                </td>
+
+                <!--Segunda coluna -> Falta 1° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f1b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Terceira coluna -> Nota 1° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n1b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Quarta coluna -> Falta 2° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f2b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Quinta coluna -> Nota 2° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n2b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Sexta coluna -> Falta 3° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f3b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Sétima coluna -> Nota 3° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n3b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Oitava coluna -> Falta 4° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f4b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Nona coluna -> Nota 4° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n4b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Décima coluna -> Falta recuperação-->
+                <td>
+                    <input min="0" type="number" step="1" name="f4b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Décima primeira coluna -> Nota recuperação-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n3b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Falta final-->
+                <td>
+                    <h3>
+                        <font color="gray" title="A falta final ainda não está disponível, já que nem todas as faltas foram lançadas.">-:--</font>
+                    </h3>
+                </td>
+
+                <!--Nota final-->
+                <td>
+                    <h3>
+                        <font color="gray" title="A nota final ainda não está disponível, já que nem todas as notas foram lançadas.">-,-</font>
+                    </h3>
+                </td>
+
+                <!--Situação-->
+                <td>
+                    <h3>
+                        <font color="gray" title="A situação do aluno ainda está em andamento, já que nem todas as notas e faltas foram publicadas.">Em andamento</font>
+                    </h3>
+                </td>
+
+            </tr>
+
+
+            <!-- Registro 3 -->
+            <tr class="linha_registros">
+
+                <!--Primeira coluna -> Aluno-->
+                <td class="nome">
+                    <h3>Bruno Muniz de Carvalho</h3>
+                </td>
+
+                <!--Segunda coluna -> Falta 1° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f1b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Terceira coluna -> Nota 1° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n1b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Quarta coluna -> Falta 2° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f2b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Quinta coluna -> Nota 2° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n2b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Sexta coluna -> Falta 3° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f3b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Sétima coluna -> Nota 3° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n3b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Oitava coluna -> Falta 4° bim-->
+                <td>
+                    <input min="0" type="number" step="1" name="f4b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Nona coluna -> Nota 4° bim-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n4b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Décima coluna -> Falta recuperação-->
+                <td>
+                    <input min="0" type="number" step="1" name="f4b" id="i" value="" placeholder="-" disabled="">
+                </td>
+
+                <!--Décima primeira coluna -> Nota recuperação-->
+                <td>
+                    <input min="0" type="number" step="0.5" name="n3b" id="i" value="" placeholder="-,-" disabled="">
+                </td>
+
+                <!--Falta final-->
+                <td>
+                    <h3>
+                        <font color="green">4</font>
+                    </h3>
+                </td>
+
+                <!--Nota final-->
+                <td>
+                    <h3>
+                        <font color="red">40,0</font>
+                    </h3>
+                </td>
+
+                <!--Situação-->
+                <td>
+                    <h3>
+                        <font color="red">Reprovado</font>
+                    </h3>
+                </td>
+
+            </tr>
+
+            <!-- ***** Parte dinâmica acima*****-->
+
         </table>
+    </form>
+        
 </body>
 </html>
