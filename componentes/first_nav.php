86d50290151df388bb6f8f54264c4dc4d13cbaf5
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-11-08 17:13:43 -0300

    Quebra de linha no título do nome da escola no menu do site e customização e aplicação de efeito hover e active no mesmo

--- componentes/first_nav.php
+++ componentes/first_nav.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
-    <header>
-      <div class="tudo">
-        <div class="info">
-          <a href="./index.php">
-            <img class="logo" src="./img/logo_juvenal.png" alt="Logo da Escola Estadual Professor Juvenal Brandão"/>
-          </a>
-          <font class="titulo"><a href="./index.php" style="text-decoration: none; color: inherit;">Escola Estadual Professor Juvenal Brandão</a></font>
-        </div>
-        <a href="login.php">
-          <button class="botao">ACESSE O SIEGE</button>
-        </a>
-      </div>
-    </header>
-    <section>
-      <div class="menu">
-        <ul class="nav justify-content-center" style="background-color:#191970">
-        <li class="nav-item">
-            <a class="nav-link text-white" href="index.php">Início</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="noticias.php">Notícias</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="boas_praticas.php">Boas Práticas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="editais.php">Editais</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="como_acessar.php">Como acessar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="sobre.php">Sobre nós</a>
-          </li>
-        </ul>
-      </div>
-    </section>
+<header>
+  <div class="tudo">
+    <div class="info">
+      <a href="./index.php">
+        <img class="logo" src="./img/logo_juvenal.png" alt="Logo da Escola Estadual Professor Juvenal Brandão" />
+      </a>
+      <font class="titulo"><a href="./index.php" style="text-decoration: none; color: inherit;">Escola Estadual<br>Professor Juvenal Brandão</a></font>
+    </div>
+    <a href="login.php">
+      <button class="botao">ACESSE O SIEGE</button>
+    </a>
+  </div>
+</header>
+<section>
+  <div class="menu">
+    <ul class="nav justify-content-center" style="background-color:#191970">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="index.php">Início</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="noticias.php">Notícias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="boas_praticas.php">Boas Práticas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="editais.php">Editais</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="como_acessar.php">Como acessar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="sobre.php">Sobre nós</a>
+      </li>
+    </ul>
+  </div>
+</section>
