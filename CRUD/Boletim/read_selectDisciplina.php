acf076af3f6c3f341537d1ff45b651fd34dd6583
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-11-27 20:24:58 -0300

    Foi colocado um ORDER BY nome ASC para o read_selectDisciplina

--- CRUD/Boletim/read_selectDisciplina.php
+++ CRUD/Boletim/read_selectDisciplina.php
@@ -1,18 +1,17 @@
 <?php
-    include("../../BancoDados/conexao_mysql.php");
+include("../../BancoDados/conexao_mysql.php");
 
-    if(isset($_GET['idt'])){
-        $prepara2 = $conexao->prepare("SELECT id, nome FROM disciplina WHERE id_turma = ?");
-        $prepara2->bind_param("i", $_GET['idt']);
-        $prepara2->execute();
-        $resultado2 = $prepara2->get_result();
-        $disciplinas = array();
-        $i = 0;
-        while ($d = $resultado2->fetch_object()) {
-            $disciplinas[$i] = $d;
-            $i++;
-        }
-
-        echo json_encode($disciplinas);
+if (isset($_GET['idt'])) {
+    $prepara2 = $conexao->prepare("SELECT id, nome FROM disciplina WHERE id_turma = ? ORDER BY nome ASC");
+    $prepara2->bind_param("i", $_GET['idt']);
+    $prepara2->execute();
+    $resultado2 = $prepara2->get_result();
+    $disciplinas = array();
+    $i = 0;
+    while ($d = $resultado2->fetch_object()) {
+        $disciplinas[$i] = $d;
+        $i++;
     }
-?>
+
+    echo json_encode($disciplinas);
+}
