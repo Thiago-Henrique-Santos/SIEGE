d12730b97a19f75ce22010c1bd35d8196af89b18
NatanFS <natan.fernandes@alunos.ifsuldeminas.edu.br>
2021-12-05 23:38:06 -0300

    Foram colocados alguns efeitos hover no rodapé do site e colocado o link para ancora no rodapé e no menu do site

--- componentes/first_footer.php
+++ componentes/first_footer.php
@@ -8,33 +8,33 @@
 			<div id=div_escola class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
 				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">A escola</h5>
 				<p>A E.E. Professor Juvenal Brandão oferece toda a estrutura necessária para o conforto e desenvolvimento educacional dos seus alunos, como por exemplo: Internet, Banda Larga, Reciclagem de Lixo, Refeitório, Biblioteca, Quadra Esportiva, Laboratório de Informática, Sala do Professor e Alimentação.</p>
-				
+
 			</div>
 
 			<div id=div_acesso class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
 				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">Acesso Rápido</h5>
-			<p>
-				<a href="index.php" class="text-white" style="text-decoration: none;">Início</a>
-			</p>
-			<p>
-				<a href="conquistas.php" class="text-white" style="text-decoration: none;">Conquistas</a>
-			</p>
-			<p>
-				<a href="boas_praticas.php" class="text-white" style="text-decoration: none;">Boas Práticas</a>
-			</p>
-			<p>
-				<a href="editais.php" class="text-white" style="text-decoration: none;">Editais</a>
-			</p>
-			<p>
-				<a href="como_acessar.php" class="text-white" style="text-decoration: none;">Como Acessar</a>
-			</p>
-			<p>
-				<a href="sobre.php" class="text-white" style="text-decoration: none;">Sobre Nós</a>
-			</p>
+				<p>
+					<a class='link_acesso_rapido' href="index.php" class="text-white" style="text-decoration: none;">Início</a>
+				</p>
+				<p>
+					<a class='link_acesso_rapido' href="#conquistas" class="text-white" style="text-decoration: none;">Conquistas</a>
+				</p>
+				<p>
+					<a class='link_acesso_rapido' href="#boas_praticas" class="text-white" style="text-decoration: none;">Boas Práticas</a>
+				</p>
+				<p>
+					<a class='link_acesso_rapido' href="#editais" class="text-white" style="text-decoration: none;">Editais</a>
+				</p>
+				<p>
+					<a class='link_acesso_rapido' href="como_acessar.php" class="text-white" style="text-decoration: none;">Como Acessar</a>
+				</p>
+				<p>
+					<a class='link_acesso_rapido' href="sobre.php" class="text-white" style="text-decoration: none;">Sobre Nós</a>
+				</p>
 			</div>
 
 			<div id=div_contato class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-				<h5 class="text-uppercase mb-4 font-weight-bold text-warning" >Contato</h5>
+				<h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contato</h5>
 				<p>
 					<i class="fas fa-home mr-3"></i><br>Rua Engenheiro Américo Guidi Filho, 48 - Centro, Ouro Fino - MG
 				</p>
@@ -42,10 +42,10 @@
 					<i class="fas fa-envelope mr-3"></i><br>escola.55964@educacao.mg.gov.br
 				</p>
 				<p>
-					<i class="fas fa-phone mr-3"></i><br>(35) 3441-1247
+					<i class="fas fa-phone-alt"></i><br>(35) 3441-1247
 				</p>
 			</div>
-			
+
 		</div>
 
 		<hr class="mb-4">
@@ -55,7 +55,7 @@
 			<div class="col-md-7 col-lg-8">
 				<p>Escola Estadual Professor Juvenal Brandão</p>
 				<p>Desenvolvido por alunos do IF Sul de Minas - Campus Inconfidentes - 2021</p>
-				
+
 			</div>
 
 			<div class="col-md-5 col-lg-4">
@@ -63,22 +63,22 @@
 
 					<ul class="list-unstyled list-inline">
 						<li class="list-inline-item">
-							<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiRge26y8r0AhVwrZUCHcjsAOYQFnoECAMQAQ&url=https%3A%2F%2Fwww.facebook.com%2Fescolajuvenalbrandao%2F&usg=AOvVaw2oUj1GJVOaerVPWSpPf_Bd" target="_blank" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
+							<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiRge26y8r0AhVwrZUCHcjsAOYQFnoECAMQAQ&url=https%3A%2F%2Fwww.facebook.com%2Fescolajuvenalbrandao%2F&usg=AOvVaw2oUj1GJVOaerVPWSpPf_Bd" target="_blank" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i id='btn_facebook' class="fab fa-facebook"></i></a>
 						</li>
 						<li class="list-inline-item">
-							<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjilr75y8r0AhWBrJUCHWpnBrAQFnoECAoQAQ&url=https%3A%2F%2Fwww.instagram.com%2Fescolajuvenalbrandao%2F&usg=AOvVaw3gIwfieAGalffDibtOMW5d" target="_blank" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+							<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjilr75y8r0AhWBrJUCHWpnBrAQFnoECAoQAQ&url=https%3A%2F%2Fwww.instagram.com%2Fescolajuvenalbrandao%2F&usg=AOvVaw3gIwfieAGalffDibtOMW5d" target="_blank" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i id='btn_instagram' class="fab fa-instagram"></i></a>
 						</li>
 						<li class="list-inline-item">
-							<a href="https://mail.google.com/mail/u/0/#inbox?compose=VpCqJRxbqNpbJTlHjPbPLXlkbpdKZXSvCHnfklRpsVJqsfwxbVXLTmDQvhkzNbZsxLDfKbB" target="_blank" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fa fa-envelope"></i></a>
+							<a href="https://mail.google.com/mail/u/0/#inbox?compose=VpCqJRxbqNpbJTlHjPbPLXlkbpdKZXSvCHnfklRpsVJqsfwxbVXLTmDQvhkzNbZsxLDfKbB" target="_blank" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i id='btn_email' class="fa fa-envelope"></i></a>
 						</li>
 					</ul>
-					
+
 				</div>
-				
+
 			</div>
-			
+
 		</div>
 
 	</div>
-	
+
 </footer>
