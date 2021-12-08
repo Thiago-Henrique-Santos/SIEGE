5573c93a72e2a5eb012f10a135a02c48dfc4c1f5
Thiago-Henrique-Santos <thilego418@gmail.com>
2021-12-08 12:57:35 -0200

    Bug Fixed - Corrigingo bug do recurso de alerta caso o usuário acesse uma matéria inexiste (bug estava caso ['materia'] não estivesse setado

--- saiba_mais.php
+++ saiba_mais.php
@@ -9,7 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="img/logo_juvenal.png" />
     <?php
-    $materia = $_GET['materia'];
+    if (isset($_GET['materia'])) {
+        $materia = $_GET['materia'];
+    } else {
+        $materia = false;
+    }
+
     switch ($materia) {
         case 'EducacaoParaAVida':
             echo "<title> Educação para a Vida </title>";
