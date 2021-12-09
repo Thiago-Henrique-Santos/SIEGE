48a36a70b3b426c231c97839cd8787f616db9467
Ana Eliza Feitoza <ana.eliza@alunos.ifsuldeminas.edu.br>
2021-12-08 22:27:06 -0300

    Página sobre

--- sobre.php
+++ sobre.php
@@ -29,10 +29,82 @@
         <main>
             <div style="width: 100%; height: 50px; display: flex; 
             flex-flow: row nowrap; justify-content: center;">
-                <h1 class="titulo-principal"> Sobre a escola </h1>
+                <h1 class="titulo-principal"> Histórico da Escola </h1>
             </div>
         </main>
     </div>
+    <p>
+    A Nossa escola foi criada pelo decreto nº 4.227, de 1º de junho de 1954. 
+    Sua instalação ocorreu em 19 de março de 1965, com o nome de Grupo Escolar 
+    “Professor Juvenal Brandão”. O Patrono escolhido é o Professor Juvenal Sanches 
+    de Lemos Brandão, que sempre se dedicou às causas relacionadas à Educação.
+    A escola iniciou seu funcionamento em um prédio alugado, à Rua General Osório, nº 64. 
+    Com o esforço conjunto da diretoria e comunidade escolar, finalmente, no dia 19 de março 
+    de 1972, inaugurou-se o novo prédio com o nome de “E.E. Professor Juvenal Brandão”, onde 
+    funciona até os dias de hoje, à Rua Engenheiro Américo Guide Filho, nº 48.
+    </p>
+
+    <h3> Juvenal Sanches de Lemos Brandão</h3>
+    
+    <p>
+    Desde criança, manifestou desejo de estudar e como residia com seus pais na Fazenda Bom Viver, vem para a zona urbana morar com seus parentes, no Solar de Cel Paiva, onde permaneceu, convivendo com seu primo, Julio Bueno Brandão, com o qual concluiu o curso primário, com o velho professor João Carlos Schimidt.
+    Ao terminar seu curso de humanidades no Colégio da cidade de Campo Belo, matriculou-se na Faculdade de Medicina no Rio de Janeiro, no entanto uma epidemia de febre amarela, o fez regressar à Ouro Fino novamente, onde lecionou Ciências em um colégio particular.
+    Seu ideal em se formar em uma escola superior o impulsiona a migrar para São Paulo e matricular-se na faculdade de Direito. Porém com a morte de seu pai, teve que assumir a Fazenda da família e deixar, por hora, seu sonho de lado.
+    </p>
+    <center>
+         <div class='col-sm-6'>
+             <div class='card mb-3'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <img src='img/sobre1.jpg' class='card-img-top' alt='...'>
+                        <p class='card-text'>De volta ao passado: Fotos da Escola.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='col-sm-6'>
+             <div class='card mb-3'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <img src='img/sobre2.jpg' class='card-img-top' alt='...'>
+                        <p class='card-text'>De volta ao passado: Fotos da Escola.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </center>
+    <p>
+    Na cidade, Juvenal se dedica ao magistério, enfrentando todas as dificuldades, funda um colégio em Ouro Fino, denominado, Instituto Municipal, talvez um dos primeiros do Estado de Minas Gerais.
+    Com o passar do tempo começou a lecionar Matemática e Línguas em outras escolas. Nesta época conheceu sua esposa, Etelvina. Só deixou a sala de aula quando foi nomeado pelo governador do estado, Inspetor Técnico de Ensino.
+    Essa nomeação fez com que o Senhor Juvenal viajasse constantemente pelo Sul de Minas, onde fiscalizava e normalizava o ensino. Enfrentando divergências, mas nuca desistindo, conseguiu solucionar muitas divergências, demonstrando características diplomáticas. Durante essas viagens ele chegou à conclusão de que muitas professoras não tinham formação pedagógica, assim havendo uma necessidade urgente de adaptação dos programas de ensino. Essa ideia foi discutida em Congressos e à partir daí foram criadas as “Escolas Normais”.
+    </p>
+    <center>
+         <div class='col-sm-6'>
+             <div class='card mb-3'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <img src='img/sobre3.jpg' class='card-img-top' alt='...'>
+                        <p class='card-text'>De volta ao passado: Fotos da Escola.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='col-sm-6'>
+             <div class='card mb-3'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <img src='img/sobre4.jpg' class='card-img-top' alt='...'>
+                        <p class='card-text'>De volta ao passado: Fotos da Escola.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </center>
+    <p>
+    O professor Juvenal Brandão era um homem de visão e comprometido com sua época para a expansão e atualização do ensino em Minas Gerais. Previu a necessidade de auxílio financeiro aos alunos sem recursos suficientes para se manterem na escola, à princípio com as Caixas-Escolares.
+    Ao longo dos anos colaborou com revistas de Ensino, debatendo soluções de problemas educacionais, sendo também diretor da E. E. Coronel Paiva.
+    Por sua dedicação à causa de ensino e como exemplo de educador íntegro e um verdadeiro idealista, foi escolhido para patrono de nossa querida escola.
+    </p>
 
     <?php
     include('componentes/first_footer.php');
